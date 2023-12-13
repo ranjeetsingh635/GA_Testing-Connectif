6cac462e5066e6aef6067019e1defbbac104b9b5
Admin <ranjeetsinghmobile>
2023-12-13 16:17:45 +0530

    testing

--- src/Modules/LayoutModule/Components/SetupLayoutModule/Header/Header.tsx
+++ src/Modules/LayoutModule/Components/SetupLayoutModule/Header/Header.tsx
@@ -12,7 +12,7 @@ function Header() {
         </Link>
         <Link passHref href={`/create-account`}>
           <a className="primaryBtn px-[1rem] max-w-[15rem] py-[2rem] rounded-[.6rem]">
-            MukundP
+            Mukund
           </a>
         </Link>
       </div>
