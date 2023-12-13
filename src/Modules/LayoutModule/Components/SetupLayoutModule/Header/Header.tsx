09f82b0448ac5ab0e169dcd161dab9ca9d033f93
Admin <ranjeetsinghmobile>
2023-12-13 14:45:10 +0530

    testing2

--- src/Modules/LayoutModule/Components/SetupLayoutModule/Header/Header.tsx
+++ src/Modules/LayoutModule/Components/SetupLayoutModule/Header/Header.tsx
@@ -12,7 +12,7 @@ function Header() {
         </Link>
         <Link passHref href={`/create-account`}>
           <a className="primaryBtn px-[1rem] max-w-[15rem] py-[2rem] rounded-[.6rem]">
-            Mobile1110
+            Bhupendraji
           </a>
         </Link>
       </div>
