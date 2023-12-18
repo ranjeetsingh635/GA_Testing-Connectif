513130665c1e8ca1a908d997b080031e42c647c4
Admin <ranjeetsinghmobile>
2023-12-18 14:55:37 +0530

    jeet

--- src/Modules/LayoutModule/Components/SetupLayoutModule/Header/Header.tsx
+++ src/Modules/LayoutModule/Components/SetupLayoutModule/Header/Header.tsx
@@ -12,7 +12,7 @@ function Header() {
         </Link>
         <Link passHref href={`/create-account`}>
           <a className="primaryBtn px-[1rem] max-w-[15rem] py-[2rem] rounded-[.6rem]">
-            Bhup+Jeet
+            Bhup+Jeet1
           </a>
         </Link>
       </div>
