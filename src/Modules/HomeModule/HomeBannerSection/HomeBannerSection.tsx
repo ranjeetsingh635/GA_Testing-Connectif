f528a363f8e0ba06b4322da9895228bec6e3b915
ranjeetsinghmobile <92848087+ranjeetsinghmobile@users.noreply.github.com>
2025-03-26 18:13:57 +0530

    Update HomeBannerSection.tsx

--- src/Modules/HomeModule/HomeBannerSection/HomeBannerSection.tsx
+++ src/Modules/HomeModule/HomeBannerSection/HomeBannerSection.tsx
@@ -36,7 +36,7 @@ function HomeBannerSection() {
               A{" "}
               <span className=" text-light-primaryPurple-tints-900">
                 {" "}
-                Platform{" "}
+                Platform4{" "}
               </span>{" "}
               Build for New Way of{" "}
               <span className="text-light-primaryPurple-tints-900">
